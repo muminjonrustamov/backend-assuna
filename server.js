6a74f3e4620c15f8d090f850e2b5b805434c5a3d
Muminjon <119095876+muminjonrustamov@users.noreply.github.com>
2025-08-13 17:28:55 +0500

    change

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(cors({
   origin: 'https://assuna.netlify.app',
